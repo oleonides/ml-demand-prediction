85b86e26d392b855ed5d73ce56b819a58b787e7c
Olvert Leonides <olvert.leonides@gmail.com>
2023-08-27 02:10:54 -0600

    refactoring code

--- app.py
+++ app.py
@@ -57,10 +57,11 @@ def show_data_shape(data):
     st.write(data.shape[1])
 
 #################### Forecasting ####################
+
+
 def plot_raw_data(data):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['ds'],
-                  y=data['y'], name="stock_open"))
+    fig.add_trace(go.Scatter(x=data['ds'], y=data['y']))
     fig.layout.update(
         title_text='Time series data with range slider', xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
@@ -75,12 +76,16 @@ def predict_demand_prophet(data: pd.DataFrame):
     ds = st.selectbox("Select DS variable", [None] + columns)
     y = st.selectbox("Select y variable", [None] + columns)
 
-    if ds is not None and y is not None:
+    if ds is not None and y is not None and ds != y:
         df_train = data[[ds, y]]
 
-        remove_negative_values = st.checkbox("Remove negative values")
-        if remove_negative_values:
+        y_options = [None, "Remove negative values", "Take absolute values"]
+        y_selected_option = st.radio("Select an option", y_options)
+        if y_selected_option is not None and y_selected_option == "Remove negative values":
+            df_train = df_train[df_train[y] >= 0]
+        if y_selected_option is not None and y_selected_option == "Take absolute values":
             df_train[y] = abs(df_train[y])
+
         group_values = st.checkbox("Group values by date")
         if group_values:
             df_train[ds] = pd.to_datetime(df_train[ds]).dt.strftime('%Y-%m-%d')
@@ -90,22 +95,23 @@ def predict_demand_prophet(data: pd.DataFrame):
         st.write(df_train)
         plot_raw_data(df_train)
 
-        m = Prophet()
-        m.fit(df_train)
-        future = m.make_future_dataframe(periods=period)
-        forecast = m.predict(future)
+        start = st.checkbox("Start forecasting")
+        if start:
+            m = Prophet()
+            m.fit(df_train)
+            future = m.make_future_dataframe(periods=period)
+            forecast = m.predict(future)
 
-        # Show and plot forecast
-        st.subheader('Forecast data')
-        st.write(forecast.tail())
+            st.subheader('Forecast data')
+            st.write(forecast.tail())
 
-        st.write(f'Forecast plot for {n_years} years')
-        fig1 = plot_plotly(m, forecast)
-        st.plotly_chart(fig1)
+            st.write(f'Forecast plot for {n_years} years')
+            fig1 = plot_plotly(m, forecast)
+            st.plotly_chart(fig1)
 
-        st.write("Forecast components")
-        fig2 = m.plot_components(forecast)
-        st.write(fig2)
+            st.write("Forecast components")
+            fig2 = m.plot_components(forecast)
+            st.write(fig2)
 
 
 def predict_demand_xgboost(data):
